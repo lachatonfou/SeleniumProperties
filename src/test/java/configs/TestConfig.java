756e8035b9848d5c2dc69f0926c01c640bc92186
lachatonfou <maniakitten@mail.ru>
2025-04-11 17:29:08 +1000

    урок 8

---
+++ src/test/java/configs/TestConfig.java
@@ -0,0 +1,43 @@
+package configs;
+
+//import static constants.Constants.BASE_URL;
+//
+//public class TestConfig {
+//    public String getBaseUrl() {
+//        return System.getProperty("baseUrl", BASE_URL);
+//    }
+//}
+
+import java.io.IOException;
+import java.util.Properties;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class TestConfig {
+    String env;
+    Properties properties;
+
+    public TestConfig() {
+        env = System.getProperty("env", "default");
+        properties = getPropertiesByEnv(env);
+    }
+
+    public String getBaseUrl() {
+        String baseUrl = properties.getProperty("baseUrl");
+        assertNotNull(baseUrl, String.format("BaseUrl is not found in %s.properties", env));
+        System.out.println("Base URL: " + baseUrl);
+        return baseUrl;
+    }
+
+    private Properties getPropertiesByEnv(String env) {
+        Properties testProperties = new Properties();
+        try {
+            testProperties.load(getClass().getClassLoader().getResourceAsStream(env + ".properties"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (Exception e) {
+            throw new RuntimeException(String.format("Cannot open %s.properties", env));
+        }
+        return testProperties;
+    }
+}
